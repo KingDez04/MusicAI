42466dbd00b2675aca00c95bbbd1c86645b0b28b
Ademola Ademeso <desmondademeso@gmail.com>
2024-08-25 15:59:09 +0100

    Disabled button when loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Content } from "./components/Content";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Content />
       <div className="flex flex-row max-w-sm justify-around mx-auto mt-4">
         <a
@@ -10,17 +10,17 @@ const App = () => {
           href="https://github.com/KingDez04/MusicAI"
           target="_blank"
         >
-          Source Code
+          Code
         </a>
         <a
           className="hover:text-red-700"
           href="https://ademolaademeso.vercel.app"
           target="_blank"
         >
-          Personal Website
+          Portfolio
         </a>
       </div>
-    </div>
+    </>
   );
 };
 
