f8bb6680c9c32e3bd92a20363399d94e86397c34
Ademola Ademeso <desmondademeso@gmail.com>
2024-08-31 23:09:46 +0100

    Changed setup from dev to prod

---
+++ server/api/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const OpenAI = require("openai");
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const aiModel = "gpt-3.5-turbo";
+
+module.exports = async (req, res) => {
+  const { inputs } = req.query;
+
+  if (inputs) {
+    const prompt = [
+      "You are an expert in recommending music.",
+      "Based on the following inputs, suggest me a good song to listen.",
+      inputs,
+    ].join(" ");
+
+    const messages = [{ role: "system", content: prompt }];
+
+    try {
+      const completion = await openai.chat.completions.create({
+        model: aiModel,
+        messages,
+      });
+
+      const aiResponse = completion.choices[0].message.content;
+      return res.json({ aiResponse });
+    } catch (error) {
+      console.error("Error with OpenAI API:", error);
+      return res.status(500).json({ error: "Error fetching recommendation." });
+    }
+  } else {
+    return res.status(400).json({ message: "No input provided" });
+  }
+};
